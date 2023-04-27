d10672c4aa4d2ec256a66f0e3b2eb7ef0abcbd2f
Bernardo_Marconi <126028051+Bernardo-Marconi@users.noreply.github.com>
2023-04-27 20:24:36 -0300

    lista-quatro-java-una
    Revisão - Exercício 01 - Sistema de Identificação de Risco de Infecção por Vírus Influenza H5N1.

--- ExercicioDeRevisao.java
+++ ExercicioDeRevisao.java
@@ -2,21 +2,16 @@ import java.util.Scanner;
 
 public class ExercicioDeRevisao {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         
         System.out.println("Revisão - Exercício 01");
 
-        String cartaoVacina;
-        String sintomas;
-        String contato;
-        String retornoViagem;
+        String[] pergunta = new String[4];
 
-        int cartaoVacinaResposta=0;
-        int sintomasResposta=0;
-        int contatoResposta=0;
-        int retornoViagemResposta=0;
+        byte[] porcentagem = {0,0,0,0};
 
-        int[] tentativas = {0,0,0,0};
+        byte[] tentativas = {0,0,0,0};
 
         System.out.println("Informe o seu nome: ");
         String nome = scan.nextLine();
@@ -26,85 +21,91 @@ public class ExercicioDeRevisao {
 
         scan.nextLine();
         
-        System.out.println("\nResponda as perguntas com 'SIM' ou 'NAO': ");
+        System.out.println("Responda as perguntas com 'SIM' ou 'NAO': ");
 
     do {
         System.out.println("Seu cartão de vacina está em dia?");
-        cartaoVacina=scan.nextLine();
+        pergunta[0]=scan.nextLine();
 
         tentativas[0]++;
 
-    } while (tentativas[0] <= 3 && !cartaoVacina.equals("sim") && !cartaoVacina.equals("nao"));
+    } while (tentativas[0] <= 2 && !pergunta[0].equals("sim") && !pergunta[0].equals("nao"));
 
-    if (tentativas[0] == 4) {
+    if (tentativas[0] == 3) {
 
         System.out.println("Não foi possível realizar o diagnóstico. Gentileza procurar ajuda médica caso apareça algum sintoma.");
 
-    } else {
+        System.exit(0);
+
+    }
 
     do { 
         System.out.println("Teve algum dos sintomas recentemente? (dor de cabeça, febre, náusea, dor articular, gripe)");
-        sintomas = scan.nextLine();
+        pergunta[1] = scan.nextLine();
 
         tentativas[1]++;
 
-    } while (tentativas[1] <= 3 && !sintomas.equals("sim") && !sintomas.equals("nao"));
+    } while (tentativas[1] <= 2 && !pergunta[1].equals("sim") && !pergunta[1].equals("nao"));
 
-    if (tentativas[1] == 4) {
+    if (tentativas[1] == 3) {
 
         System.out.println("Não foi possível realizar o diagnóstico. Gentileza procurar ajuda médica caso apareça algum sintoma.");
     
-    } else {
+        System.exit(0);
+
+    }
 
     do {
         System.out.println("Teve contato com pessoas com sintomas gripais nos últimos dias?");
-        contato = scan.nextLine();
+        pergunta[2] = scan.nextLine();
 
         tentativas[2]++;
 
-    } while (tentativas[2] <= 3 && !contato.equals("sim") && !contato.equals("nao"));
+    } while (tentativas[2] <= 3 && ! pergunta[2].equals("sim") && ! pergunta[2].equals("nao"));
         
     if (tentativas[2] == 4) {
 
         System.out.println("Não foi possível realizar o diagnóstico. Gentileza procurar ajuda médica caso apareça algum sintoma.");
     
-    } else {
-    
+        System.exit(0);
 
+    }
+    
     do {
         System.out.println("Está retornando de viagem realizada no exterior?");
-        retornoViagem = scan.nextLine();
+        pergunta[3] = scan.nextLine();
 
         tentativas[3]++;
 
-    } while (tentativas[3] <= 3 && !retornoViagem.equals("sim") && !retornoViagem.equals("nao"));
+    } while (tentativas[3] <= 3 && !pergunta[3].equals("sim") && !pergunta[3].equals("nao"));
 
     if (tentativas[3] == 4) {
 
         System.out.println("Não foi possível realizar o diagnóstico. Gentileza procurar ajuda médica caso apareça algum sintoma.");
     
-    } else {
+        System.exit(0);
+
+    }
     
+    if (pergunta[0].equals( "nao")){
 
-    if (cartaoVacina.equals( "nao")){
+        porcentagem[0]=10;
 
-        cartaoVacinaResposta=10;
+    } if (pergunta[1].equals( "sim")){
 
-    } if (sintomas.equals( "sim")){
+        porcentagem[1]=30;
 
-        sintomasResposta=30;
+    } if (pergunta[2].equals( "sim")){
 
-    } if (contato.equals( "sim")){
+        porcentagem[2]=30;
 
-        contatoResposta=30;
+    } if (pergunta[3].equals( "sim")){
 
-    } if (retornoViagem.equals( "sim")){
-
-        retornoViagemResposta=30;
+        porcentagem[3]=30;
 
     }
 
-        int resultado=(cartaoVacinaResposta+sintomasResposta+contatoResposta+retornoViagemResposta);
+        int resultado=(porcentagem[0]+porcentagem[1]+porcentagem[2]+porcentagem[3]);
 
         System.out.println("\nRelatório final: ");
     
@@ -112,19 +113,19 @@ public class ExercicioDeRevisao {
 
         System.out.println("Idade: "+idade);
 
-        System.out.println("O cartão de vacina está em dia? "+cartaoVacina);
+        System.out.println("O cartão de vacina está em dia? "+pergunta[0]);
 
-        System.out.println("Teve sintomas recentemente? "+sintomas);
+        System.out.println("Teve sintomas recentemente? "+pergunta[1]);
 
-        System.out.println("Teve contato com pessoas infectadas? "+contato);
+        System.out.println("Teve contato com pessoas infectadas? "+pergunta[2]);
 
-        System.out.println("Está retornando de viagem?"+retornoViagem);
+        System.out.println("Está retornando de viagem?"+pergunta[3]);
 
         System.out.printf("Probabilidade de infecção: %d%%\n",resultado);
 
         System.out.println("Orientação final do atendimento: ");
 
-    if (retornoViagem.equals( "sim")){
+    if (pergunta[3].equals( "sim")){
 
         System.out.printf("Você ficará sob observação por 05 dias, por está retornando de viagem.");
 
@@ -146,15 +147,8 @@ public class ExercicioDeRevisao {
 
         System.out.println("Paciente crítico! Gentileza aguardar em lockdown por 10 dias para ser acompanhado.");
 
-                     }
-                 } 
-            }
-        }
-    }
-}
-
-        scan.close();
-
+               } 
+            scan.close();
+        } 
     } 
-
 }
